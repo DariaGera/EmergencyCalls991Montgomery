9849d64b29bef63e68ad8261ab81ece37e3d6fd4
DariaGera <43523466+DariaGera@users.noreply.github.com>
2020-05-05 23:39:02 +0300

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -5,6 +5,7 @@ insert into town(town) values('PERKIOMEN');
 
 insert into TimeStampp(TimeStampp) values(to_timestamp('10-JUL-19 08.46.01.000000000 AM','DD-MON-RR HH.MI.SSXFF AM'));
 insert into TimeStampp(TimeStampp) values(to_timestamp('25-JUL-19 03.17.41.000000000 PM','DD-MON-RR HH.MI.SSXFF AM'));
+insert into TimeStampp(TimeStampp) values(to_timestamp('25-JUL-18 03.17.41.000000000 PM','DD-MON-RR HH.MI.SSXFF AM'));
 
 insert into title(title) values('Fire: BUILDING FIRE');
 insert into title(title) values('Traffic: VEHICLE ACCIDENT -');
@@ -14,6 +15,7 @@ insert into Address(address) values('FAYETTE ST E 5TH AVE');
 insert into Address(address) values('BORO LINE RD DEAD END');
 insert into Address(address) values('GODSHALL RD BROOKSIDE RD');
 
+insert into accident(TOWN,ADDRESS,TIMESTAMPP,TITLE) values ('NORRISTOWN','WALNUT ST E FORNANCE ST',to_timestamp('25-JUL-18 03.17.41.000000000 PM','DD-MON-RR HH.MI.SSXFF AM'),'Traffic: VEHICLE ACCIDENT -');
 insert into accident(TOWN,ADDRESS,TIMESTAMPP,TITLE) values ('NORRISTOWN','WALNUT ST E FORNANCE ST',to_timestamp('10-JUL-19 08.46.01.000000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'Traffic: VEHICLE ACCIDENT -');
 insert into accident(TOWN,ADDRESS,TIMESTAMPP,TITLE) values ('PERKIOMEN','GODSHALL RD BROOKSIDE RD',to_timestamp('10-JUL-19 08.46.01.000000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'Fire: BUILDING FIRE');
 insert into accident(TOWN,ADDRESS,TIMESTAMPP,TITLE) values ('CONSHOHOCKEN','FAYETTE ST E 5TH AVE',to_timestamp('10-JUL-19 08.46.01.000000000 AM','DD-MON-RR HH.MI.SSXFF AM'),'Fire: BUILDING FIRE');
